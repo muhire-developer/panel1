86075044b13ff8fd030ebbd8d033697fb332d726
sebunyenyeri <132575071+muhire-developer@users.noreply.github.com>
2025-05-22 08:47:20 -0700

    add folder
    systems for national exams

---
+++ axios.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+// Create axios instance with custom config
+const instance = axios.create({
+    baseURL: 'http://localhost:5000',
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+// Add a request interceptor to add the auth token
+instance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// Add a response interceptor to handle errors
+instance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem('token');
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default instance; 
